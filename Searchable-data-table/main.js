f04c200d5b4cf3db3c10d7dc0eae6ded46d7065a
Reaxoh <64882473+Reaxoh@users.noreply.github.com>
2025-12-13 14:50:06 +0800

    ✨feat: Add product search and data expansion
    Key features include:
    - Implemented real-time product search.
    - Expanded products data
    
    Changes include:
    - Applied minor code changes.

--- Searchable-data-table/main.js
+++ Searchable-data-table/main.js
@@ -1,14 +1,33 @@
 const sortSelect = document.getElementById('sort-select');
+const searchInput = document.getElementById('search-input');
 
 const products = [
-    { name: "iPhone 17", price: 38900 },
-    { name: "Keyboard Pro", price: 1890 },
-    { name: "Laptop 13", price: 25900 },
-    { name: "Mouse X", price: 850 }
+    { name: "iPhone 17 (256GB)", price: 29900 }, 
+    { name: "iPhone 15 pro max (256GB)", price: 44900}, 
+    { name: "iPhone 12 (64GB)", price: 8158 }, 
+    { name: "Samsung Galaxy S24 Ultra (512GB)", price: 43900 }, 
+    { name: "Google Pixel 8 Pro (256GB)", price: 33900 }, 
+    { name: "Sony Xperia 1 VI (512GB)", price: 43990 }, 
+
+    { name: "iPad Pro 11吋 (Apple, 256GB)", price: 34900 }, 
+    { name: "Galaxy Tab S9 (Samsung, 128GB)", price: 19490 }, 
+    { name: "Xiaomi Pad 6 (小米, 128GB)", price: 8999 }, 
+
+    { name: "G PRO X Keyboard (Logitech)", price: 4490 }, 
+    { name: "BlackWidow V4 Pro (Razer)", price: 7500 }, 
+    { name: "K70 RGB Pro (Corsair)", price: 4690 }, 
+
+    { name: "MacBook Air M3 13吋 (Apple)", price: 35900 }, 
+    { name: "Zenbook S 13 OLED (ASUS)", price: 48000 }, 
+    { name: "XPS 13 Plus (Dell)", price: 55000 }, 
+ 
+    { name: "G PRO X Superlight 2 (Logitech)", price: 3290 }, 
+    { name: "Viper V3 Pro (Razer)", price: 4288 }, 
+    { name: "Aerox 5 Wireless (SteelSeries)", price: 2990 }
 ];
 
 function renderTable(data) {
-    const tbody = document.getElementById('productBody');
+    const tbody = document.getElementById('product-body');
     tbody.innerHTML = "";
 
     data.forEach(item => {
@@ -39,5 +58,15 @@ sortSelect.addEventListener('change', ()=> {
     if (option === 'price-desc') {
         sorted.sort((a, b) => b.price - a.price);               // 相減 b-a，結果為負數時 b 排前面 (大到小)
     }
-    renderTable(sorted);                                        // 重新渲染表格
+    renderTable(sorted);                                        // 使用排序後的資料重新渲染表格
+});
+
+searchInput.addEventListener("input", () => {
+    const keyword = searchInput.value.toLowerCase();            // 獲取輸入內容並轉換為小寫 
+
+    const filtered = products.filter(item =>                    // 過濾產品列表
+        item.name.toLowerCase().includes(keyword)               // 檢查產品名稱是否包含搜尋關鍵字
+    );
+
+    renderTable(filtered);                                      // 使用篩選後的資料重新渲染表格
 });
